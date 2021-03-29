fa99156e22b910617dec908cbbfb4b4348b31d15
Christina <christina_sohn@facinghistory.org>
2021-03-29 18:36:52 -0400

    header component

--- themes/react_example_theme/js/src/index.jsx
+++ themes/react_example_theme/js/src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// # Example 1: Simple "Hello, World" code
-ReactDOM.render(
-  <h1>Hello there - world!</h1>,
-  document.getElementById('react-app')
-);
+/* Components */
+import Header from './components/Header';
+
+const Main = () => (
+  <Header />
+)
+
+ReactDOM.render(<Main />, document.getElementById('react-app'));
