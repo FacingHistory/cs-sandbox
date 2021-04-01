c1255049c0ea1a8537141693e69436e46e2a741a
Christina <christina_sohn@facinghistory.org>
2021-04-01 13:22:38 -0400

    add useeffect and array

--- themes/react_example_theme/js/src/components/RecipesDisplay.jsx
+++ themes/react_example_theme/js/src/components/RecipesDisplay.jsx
@@ -3,18 +3,19 @@ import React, {useEffect, useState } from "react";
 const RecipesDisplay = () => {
 
   const [recipeData, setRecipeData] = useState([])
-
+  
+  useEffect (()=> {
     fetch("https://dev-cs-sandbox.pantheonsite.io/en/api/recipes?_format=json")
-    .then(res => res.json())
-    .then(data => {
-      setRecipeData(data)
-      console.log(recipeData)
-    })
+      .then(res => res.json())
+      .then(data => {
+        setRecipeData(data)
+      })
+  },[])
 
   return (
     <div>
       <h2>Here is a list of recipes:</h2>
-      {/* <div>{recipeData[0]}</div> */}
+      <div>{recipeData[0]}</div>
     </div>
   )
 }
