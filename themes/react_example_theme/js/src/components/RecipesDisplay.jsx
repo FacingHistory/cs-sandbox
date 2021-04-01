d61be620d22f2094dd3d6b0f389bf4272e1b5eaa
Christina <christina_sohn@facinghistory.org>
2021-04-01 17:00:35 -0400

    add jsx

--- themes/react_example_theme/js/src/components/RecipesDisplay.jsx
+++ themes/react_example_theme/js/src/components/RecipesDisplay.jsx
@@ -2,22 +2,22 @@ import React, {useEffect, useState } from "react";
 
 const RecipesDisplay = () => {
 
-  const [recipeData, setRecipeData] = useState("")
+  const [recipeData, setRecipeData] = useState([])
   
   useEffect (()=> {
     fetch("https://dev-cs-sandbox.pantheonsite.io/en/api/recipes?_format=json")
       .then(res => res.json())
       .then(data => {
-        console.log("This is the first recipe title from the data: ", data[0]["title"][0]["value"])
-        setRecipeData(data[0]["title"][0]["value"])
-        console.log("This is first recipe title from variable recipeData: ", recipeData) 
+        //console.log("This is the first recipe title from the data: ", data[0]["title"][0]["value"])
+        setRecipeData(data)
+        console.log("This is recipeData: ", recipeData) 
       })
   },[recipeData])
 
   return (
     <div>
       <h2>Here is a list of recipes:</h2>
-      {recipeData ? <div>{recipeData}</div> : <div>No data yet</div>}
+      {recipeData ? <div>{recipeData[0]["title"][0]["value"]}</div> : <div>Loading...</div>}
     </div>
   )
 }
