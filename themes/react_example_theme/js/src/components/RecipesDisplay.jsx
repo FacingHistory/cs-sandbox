f524f41ab67c45774e906fef6f80c07d3d96ad46
Christina <christina_sohn@facinghistory.org>
2021-04-01 11:12:15 -0400

    Recipes Display component

---
+++ themes/react_example_theme/js/src/components/RecipesDisplay.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect, useState } from "react";
+
+const RecipesDisplay = () => {
+
+  useEffect (() => {
+    fetch("https://dev-cs-sandbox.pantheonsite.io/en/api/recipes?_format=json")
+    .then(res => res.json())
+    .then(data => console.log(data))
+  })
+
+  return (
+    <div>
+      <h2>Here is a list of recipes:</h2>
+
+    </div>
+  )
+}
+
+export default RecipesDisplay;
--- themes/react_example_theme/js/src/index.jsx
+++ themes/react_example_theme/js/src/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React,  { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 /* Components */
 import Header from './components/Header';
 
 const Main = () => (
-  <Header />
-);
+    <Fragment>
+      <Header />
+      <RecipesDisplay />
+    </Fragment>
+  )
 
 ReactDOM.render(<Main />, document.getElementById('react-app'));
