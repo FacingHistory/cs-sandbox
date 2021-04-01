187f6babecf8ab5ae8fd1480aa9afc503c5d8fb3
Christina <christina_sohn@facinghistory.org>
2021-04-01 12:14:28 -0400

    show first recipe title

--- themes/react_example_theme/js/src/components/RecipesDisplay.jsx
+++ themes/react_example_theme/js/src/components/RecipesDisplay.jsx
@@ -2,15 +2,18 @@ import React, {useEffect, useState } from "react";
 
 const RecipesDisplay = () => {
 
+  const [recipeData, setRecipeData] = useState([])
+
   useEffect (() => {
     fetch("https://dev-cs-sandbox.pantheonsite.io/en/api/recipes?_format=json")
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => setRecipeData(data))
   })
 
   return (
     <div>
       <h2>Here is a list of recipes:</h2>
+      <div>{recipeData[0]["title"]}</div>
     </div>
   )
 }
