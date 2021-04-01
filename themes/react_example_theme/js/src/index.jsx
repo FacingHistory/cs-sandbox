cbae25aa6eb3f6819ead331f2826841a79c94ecf
Christina <christina_sohn@facinghistory.org>
2021-04-01 11:21:43 -0400

    install react hot loader

--- themes/react_example_theme/js/src/index.jsx
+++ themes/react_example_theme/js/src/index.jsx
@@ -1,14 +1,15 @@
 import React,  { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader/root';
 
 /* Components */
 import Header from './components/Header';
 
-const Main = () => (
+const Main = hot(() => (
     <Fragment>
       <Header />
       <RecipesDisplay />
     </Fragment>
-  )
+));
 
 ReactDOM.render(<Main />, document.getElementById('react-app'));
